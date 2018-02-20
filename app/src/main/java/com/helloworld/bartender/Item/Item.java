83d85184b4e8bf7311758df01a910f95533ad2d5
wilybear <12161553@inha.edu>
2018-02-20 22:43:16 +0900

    Added DatabaseHelper
    added DatabaseHelper
    changes in Item model

--- app/src/main/java/com/helloworld/bartender/Item/Item.java
+++ app/src/main/java/com/helloworld/bartender/Item/Item.java
@@ -5,16 +5,32 @@ package com.helloworld.bartender.Item;
  */
 
 public class Item {
-    int id;
-    String filter_name;
-    String attributes;
+    private long id;
+    private String filter_name;
+    private float blur;
+    private float focus;
+    private float aberation;
+    private float noiseSize;
 
+    public Item(){
 
-    public int getId() {
+    }
+
+    public Item(long id, String filter_name, float blur, float focus, float aberation, float noiseSize, float noiseIntensity) {
+        this.id = id;
+        this.filter_name = filter_name;
+        this.blur = blur;
+        this.focus = focus;
+        this.aberation = aberation;
+        this.noiseSize = noiseSize;
+        this.noiseIntensity = noiseIntensity;
+    }
+
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -26,13 +42,48 @@ public class Item {
         this.filter_name = filter_name;
     }
 
-    public String getAttributes() {
-        return attributes;
+    public float getBlur() {
+        return blur;
     }
 
-    public void setAttributes(String attributes) {
-        this.attributes = attributes;
+    public void setBlur(float blur) {
+        this.blur = blur;
     }
+
+    public float getFocus() {
+        return focus;
+    }
+
+    public void setFocus(float focus) {
+        this.focus = focus;
+    }
+
+    public float getAberation() {
+        return aberation;
+    }
+
+    public void setAberation(float aberation) {
+        this.aberation = aberation;
+    }
+
+    public float getNoiseSize() {
+        return noiseSize;
+    }
+
+    public void setNoiseSize(float noiseSize) {
+        this.noiseSize = noiseSize;
+    }
+
+    public float getNoiseIntensity() {
+        return noiseIntensity;
+    }
+
+    public void setNoiseIntensity(float noiseIntensity) {
+        this.noiseIntensity = noiseIntensity;
+    }
+
+    private float noiseIntensity;
+
 
 
 

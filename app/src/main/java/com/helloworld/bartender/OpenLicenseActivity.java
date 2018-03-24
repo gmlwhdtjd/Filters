7163a874d1d6d59392b59f1c09222a926fcccc12
wilybear <12161553@inha.edu>
2018-03-25 00:03:54 +0900

    설정창 기능 몇개 추가
    open license페이지 추가
    review누르면 마켓으로 이동

---
+++ app/src/main/java/com/helloworld/bartender/OpenLicenseActivity.java
@@ -0,0 +1,13 @@
+package com.helloworld.bartender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenLicenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_license);
+    }
+}

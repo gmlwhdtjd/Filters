65316acb543d12a1621b206b1b05a3dfb67cfd58
wilybear <12161553@inha.edu>
2018-05-25 13:46:07 +0900

    광고 테스트용 삭제, guidepointer위치 변경, 에니메이션 추가

--- materialintro/src/main/java/co/mobiwise/materialintro/animation/AnimationFactory.java
+++ materialintro/src/main/java/co/mobiwise/materialintro/animation/AnimationFactory.java
@@ -5,6 +5,9 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.view.View;
+import android.widget.ImageView;
+
+import co.mobiwise.materialintro.R;
 
 /**
  * Created by mertsimsek on 25/01/16.
@@ -97,15 +100,31 @@ public class AnimationFactory {
 
     public static void performAnimation(View view) {
 
+        ImageView guidePoint1 = view.findViewById(R.id.guide_pointer1);
+        ImageView guidePoint2 = view.findViewById(R.id.guide_pointer2);
+        ImageView guidePoint3 = view.findViewById(R.id.guide_pointer3);
+
+
         AnimatorSet firstDotAnimatorSet = new AnimatorSet();
 
-        ValueAnimator alpha = ObjectAnimator.ofFloat(view, "alpha", 1.0f, 0.8f);
-        alpha.setDuration(800);
-        alpha.setRepeatCount(ValueAnimator.INFINITE);
-        alpha.setRepeatMode(ValueAnimator.REVERSE);
-        alpha.setStartDelay(200);
+        ValueAnimator alpha1 = ObjectAnimator.ofFloat(guidePoint1, "alpha", 1.0f, 0.0f);
+        alpha1.setDuration(900);
+        alpha1.setRepeatCount(ValueAnimator.INFINITE);
+        alpha1.setRepeatMode(ValueAnimator.REVERSE);
+        alpha1.setStartDelay(600);
 
-        firstDotAnimatorSet.playTogether(alpha);
+        ValueAnimator alpha2 = ObjectAnimator.ofFloat(guidePoint2, "alpha", 1.0f, 0.0f);
+        alpha2.setDuration(900);
+        alpha2.setRepeatCount(ValueAnimator.INFINITE);
+        alpha2.setRepeatMode(ValueAnimator.REVERSE);
+        alpha2.setStartDelay(300);
+
+        ValueAnimator alpha3 = ObjectAnimator.ofFloat(guidePoint3, "alpha", 1.0f, 0.0f);
+        alpha3.setDuration(900);
+        alpha3.setRepeatCount(ValueAnimator.INFINITE);
+        alpha3.setRepeatMode(ValueAnimator.REVERSE);
+
+        firstDotAnimatorSet.playTogether(alpha1,alpha2,alpha3);
         firstDotAnimatorSet.start();
     }
 
